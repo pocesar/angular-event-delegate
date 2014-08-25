2bf77ad857a98f495023bde0646361cbdc303675
Paulo Cesar <pocesar@users.noreply.github.com>
2014-08-24 22:52:08 -0300

    Create event-delegate.ts

---
+++ event-delegate.ts
@@ -0,0 +1,71 @@
+module EventDelegate {
+    'use strict';
+
+    export module Directives {
+
+        class EventDelegate implements ng.IDirective {
+            restrict = 'A';
+            link: ng.IDirectiveLinkFn;
+
+            constructor(){
+                this.link = (scope, el, attr) => {
+                    var
+                        eventDelegate = scope.$eval(attr['eventDelegate']);
+
+                    angular.forEach(eventDelegate, (selector, event) => {
+
+                        el.on(event + '.eventDelegate', selector, (e) => {
+                            e.preventDefault();
+
+                            var
+                                target: ng.IAugmentedJQuery = angular.element(e.currentTarget),
+                                fn: ng.IInterpolationFunction;
+
+                            if (fn = target.data('EventDelegateTarget')) {
+                                scope.$apply(() => fn(target.scope()));
+                            }
+                        });
+
+                    });
+
+                    scope.$on('$destroy', () => {
+                        eventDelegate = null;
+                        el.off('.eventDelegate');
+                    });
+                };
+            }
+
+            static instance(){
+                return [() => new this()];
+            }
+        }
+
+        export var eventDelegate: ng.IDirective = EventDelegate.instance();
+
+        class EventDelegateTarget implements ng.IDirective {
+            restrict = 'A';
+            link: ng.IDirectiveLinkFn;
+
+            constructor($parse: ng.IParseService) {
+                this.link = (scope, el, attr) => {
+
+                    attr.$observe('eventDelegateTarget', (att) => {
+                        el.data('EventDelegateTarget', $parse(att));
+                    });
+
+                };
+            }
+
+            static instance(){
+                return ['$parse', ($parse) => new this($parse)];
+            }
+        }
+
+        export var eventDelegateTarget: ng.IDirective = EventDelegateTarget.instance();
+    }
+
+    angular.module('EventDelegate', []).directive({
+      'eventDelegateTarget': Directives.eventDelegateTarget,
+      'eventDelegate': Directives.eventDelegate
+    });
+}
